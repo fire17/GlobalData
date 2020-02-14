10cfbae2509ce00de4ea86c688964f62a6d256d3
Tami <fire17@gmail.com>
2020-02-14 16:18:12 +0200

    Update setup.py
    Globals Universe

--- setup.py
+++ setup.py
@@ -4,13 +4,13 @@ from distutils.core import setup
 setup(
   name = 'GlobalData',         # How you named your package folder (MyLib)
   packages = ['GlobalData'],   # Chose the same as "name"
-  version = '0.91',      # Start with a small number and increase it with every change you make
+  version = '0.92',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'Easy multiprocessing with GlobalData',   # Give a short description about your library
   author = 'Tami Bar',                   # Type in your name
   author_email = 'fire17@gmail.com',      # Type in your E-Mail
   url = 'https://github.com/fire17/GlobalData',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/fire17/GlobalData/archive/v_091.tar.gz',    # I explain this later on
+  download_url = 'https://github.com/fire17/GlobalData/archive/v_092.tar.gz',    # I explain this later on
   keywords = ['multiprocessing', 'global', 'dynamic', "data", "communitcation", "subscribe", "events"],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
           'glob3',
